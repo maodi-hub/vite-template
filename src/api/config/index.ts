ff2789b86f1c97c8680a9bb41b17f0367abba8ed
NPC <1748804251@qq.com>
2022-10-30 01:20:43 +0800

    axios配置修改

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -58,11 +58,17 @@ class HttpReauest {
 		)
   }
 
-  get<T>(url: string, params = {}, _object: AxiosRequestConfig = {}): Promise<ResultData<T>> {
+  get<T>(url: string, params?: object, _object: AxiosRequestConfig = {}): Promise<ResultData<T>> {
 		return this.service.get(url, { params, ..._object })
 	}
-  post<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+  post<T>(url: string, params?: object, _object: AxiosRequestConfig = {}): Promise<ResultData<T>> {
 		return this.service.post(url, params, _object)
+	}
+	put<T>(url: string, params?: object, _object: AxiosRequestConfig = {}): Promise<ResultData<T>> {
+		return this.service.put(url, params, _object);
+	}
+	delete<T>(url: string, params?: any, _object: AxiosRequestConfig = {}): Promise<ResultData<T>> {
+		return this.service.delete(url, { params, ..._object });
 	}
 }
 
